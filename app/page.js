649a87d4820eb04f7f19d36c2a233977d54f3ce4
Dharun Kumar S H <freebirdprod.official@gmail.com>
2026-06-16 13:05:00 +0530

    Slight Changes

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
 import FeaturedEvents from "@/components/FeaturedEvents";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <div>
-      <FeaturedEvents />
+      <Hero/>
+      <FeaturedEvents/>
     </div>
+
   );
 }
