7650886114f5ad8346d02fdf8324b927e2766346
Dharun Kumar S H <freebirdprod.official@gmail.com>
2026-06-20 11:53:15 +0530

    Slight Issues and Bugs Fixed

--- components/FeaturedEvents.jsx
+++ components/FeaturedEvents.jsx
@@ -117,6 +117,7 @@ export default function FeaturedEvents() {
                 src={event.image}
                 alt={event.title}
                 fill
+                sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, (max-width: 1024px) 33vw, 25vw"
                 className="object-cover group-hover:scale-110 transition duration-500"
               />
             </div>
